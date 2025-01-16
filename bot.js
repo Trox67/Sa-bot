46d0a4c4aff14abe6cf9a71a75d09ab5aa38e508
cenaver <157002758+Trox67@users.noreply.github.com>
2025-01-16 14:18:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,101 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+import turtle
 
-// Replace these with your server and channel IDs
-const SERVER_ID = '1264497356330635266';
-const CHANNEL_ID = '1272973045186953269';
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor("black")
+t.speed(10)
+t.pensize(2)
+t.pencolor("white")
 
-client.on('messageCreate', message => {
-    // Check if the message is in the specified server and channel
-    if (message.guild.id === SERVER_ID && message.channel.id === CHANNEL_ID) {
-        // Check if the message content is "sa"
-        if (message.content.toLowerCase() === 'sa') {
-            message.reply('as');
-        }
-    }
-});
 
-// Replace the token field with your bot's token
-client.login('MTI3Mjk3MjQyMTkzNDM1NDUyNQ.GW7n6s.9bKkpICRgyAQYSxZUvBvwl5Omlh0wcYvHXwCK4);
+
+def s_curve():
+    for i in range(90):
+        t.left(1)
+        t.forward(1)
+
+def r_curve():
+    for i in range(90):
+        t.right(1)
+        t.forward(1)
+
+def l_curve():
+    s_curve()
+    t.forward(80)
+    s_curve()
+
+def l_curve1():
+    s_curve()
+    t.forward(90)
+    s_curve()
+
+def half():
+    t.forward(50)
+    s_curve()
+    t.forward(90)
+    l_curve()
+    t.forward(40)
+    t.left(90)
+    t.forward(80)
+    t.right(90)
+    t.forward(10)
+    t.right(90)
+    t.forward(120) #on test
+    l_curve1()
+    t.forward(30)
+    t.left(90)
+    t.forward(50)
+    r_curve()
+    t.forward(40)
+    t.end_fill()
+
+def get_pos():
+    t.penup()
+    t.forward(20)
+    t.right(90)
+    t.forward(10)
+    t.right(90)
+    t.pendown()
+
+def eye():
+    t.penup()
+    t.right(90)
+    t.forward(160)
+    t.left(90)
+    t.forward(70)
+    t.pencolor("black")
+    t.dot(35)
+
+def sec_dot():
+    t.left(90)
+    t.penup()
+    t.forward(310)
+    t.left(90)
+    t.forward(120)
+    t.pendown()
+
+    t.dot(35)
+
+
+
+
+t.fillcolor("#306998")
+t.begin_fill()
+half()
+t.end_fill()
+get_pos()
+t.fillcolor("#FFD43B")
+t.begin_fill()
+half()
+t.end_fill()
+
+eye()
+sec_dot()
+
+
+
+def pause():
+    t.speed(2)
+    for i in range(100):
+        t.left(90)
+pause()
